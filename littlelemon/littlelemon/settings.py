a5f98360b2f23542e3453c3b9c9d08a061716fe1
Darren Ting <d1r22fgh98@gmail.com>
2024-06-06 21:17:19 +0800

    restaurant

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "littlelemon.reservation"
+    "reservation",
+    "restaurant"
 ]
 
 MIDDLEWARE = [
