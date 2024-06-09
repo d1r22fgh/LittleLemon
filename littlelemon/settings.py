8259b9bd65833114f5aeb4de2025b584d1733ff6
Darren Ting <d1r22fgh98@gmail.com>
2024-06-09 16:47:26 +0800

    render image

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from littlelemon import restaurant
+import restaurant
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
